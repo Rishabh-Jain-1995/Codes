b05bcb06fefadf94e80c2341387f9625c6ed0f6e
Rishabh-Jain-1995 <rishabhjainiitd2013@gmail.com>
2025-06-12 12:53:27 +0530

    Add files via upload

--- Task 2.py
+++ Task 2.py
@@ -1,3 +1,8 @@
-a=input('Enter your first name: ')
-b=input('Enter your last name: ')
-print('Hello, ', a+' ' +b + '! Welcome to Python program')
+i=1
+a=0
+start = 1
+end= 50
+for i in range(start,end+1):
+    a=a+i
+    i+=1
+print('The sum of numbers from', start, 'to', end, 'is:',a)
