eaf3ea7149f2f9abe4169137d8c1f22a23a208dd
Rishabh-Jain-1995 <rishabhjainiitd2013@gmail.com>
2025-06-16 12:45:36 +0530

    Add files via upload

--- Task 2.py
+++ Task 2.py
@@ -1,6 +1,22 @@
+filename = 'output.txt'
+write_data = input('Enter text to write to file:')
 
-import math
-a= int(input('Enter a number: '))
-print('Square Root:',math.sqrt(a))
-print('Logarithm:',math.log(a))
-print('Sine:',math.sin(a))
+file1 = open(filename,'w')
+file1.write(write_data+'\n')
+file1.close()
+file1.close()
+print('Data successfully written to',filename)
+
+
+append_data = input('Enter additional text to write to append:')
+
+file1 = open(filename,'a')
+file1.write(append_data+'\n')
+file1.close()
+file1.close()
+print('Data successfully appended.')
+
+file1 = open(filename,'r')
+reading = file1.read()
+print(reading)
+file1.close()
